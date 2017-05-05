c49c753b220ad53845eb956184f8c8c4a852dd84
amonjose <amonm.reis08@gmail.com>
2017-05-05 11:56:26 -0300

    atualizando codigo da aplicação

--- index.php
+++ index.php
@@ -1,19 +1,40 @@
-<?php  require "logica.php"; ?>
-<html>
 
-    <head></head>
+<?php
+    session_start();
+    if (!empty( $_GET['atividade'])) {
+        $_SESSION['atividades'][] = $_GET['atividade'];
+    }
 
-    <body>
-
-        <h1>
-
-
-            ola <?php echo "  $tituloVisualizado $nome,  $qualidadeVisualizada " ?>
+    ?>
 
 
 
-        </h1>
 
-    </body>
 
+
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+
+    <title>Document</title>
+</head>
+<body>
+
+<form action="">
+
+    <input type="text" name="atividade" placeholder="ex: comer lasanha">
+    <input type="submit" value="cadastrar">
+</form>
+
+<h2>Lista de atividades</h2>
+<ol>
+        <?php foreach ($_SESSION['atividades'] as $activ): ?>
+
+            <li><?= $activ ?></li>
+
+        <?php endforeach;?>
+
+</ol>
+</body>
 </html>
